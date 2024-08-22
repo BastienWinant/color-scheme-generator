f5bf238b8a4ce996a958faf855e3dced658e5787
Bastien Winant <bastien.winant@gmail.com>
2024-08-22 14:32:24 +0200

    collapse auth upon window resize

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const navExpandBtn = document.querySelector('#nav-expand-btn')
 const authExpandBtn = document.querySelector('#auth-expand-btn')
 const navContainer = document.querySelector('#nav-container')
 const userAuth = document.querySelector('#nav-auth-btns')
+
 export const navLoginBtn = document.querySelector('#nav-login-btn')
 export const navSignupBtn = document.querySelector('#nav-signup-btn')
 export const navLogoutBtn = document.querySelector('#nav-logout-btn')
@@ -17,13 +18,17 @@ function collapseNav() {
   navContainer.classList.remove('expanded')
 }
 
+function collapseAuth() {
+  userAuth.classList.remove('expanded')
+}
+
 function toggleAuth() {
   userAuth.classList.toggle('expanded')
 }
 
 navExpandBtn.addEventListener('click', expandNav)
-window.addEventListener('resize', collapseNav)
 
+// collapse nav when user clicks outside of it
 navContainer.addEventListener('click', e => {
   const isNav = e.target.closest('.nav')
 
@@ -31,5 +36,10 @@ navContainer.addEventListener('click', e => {
     collapseNav()
   }
 })
-
 authExpandBtn.addEventListener('click', toggleAuth)
+
+// collapse upon window resize
+window.addEventListener('resize', () => {
+  collapseNav()
+  collapseAuth()
+})
