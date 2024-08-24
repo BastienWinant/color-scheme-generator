cb9a5e531e8e08f6187987c5236c9be7dbaa3cf4
Bastien Winant <bastien.winant@gmail.com>
2024-08-24 19:10:06 +0200

    remove commented code and unused selectors

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,13 +4,6 @@ import { openLoginModal, logOut } from '../userAuth/login'
 import { openSignupModal } from '../userAuth/signup'
 
 const nav = document.querySelector('#nav')
-const navLoginBtn = document.querySelector('#nav-login-btn')
-const navSignupBtn = document.querySelector('#nav-signup-btn')
-const navLogoutBtn = document.querySelector('#nav-logout-btn')
-
-// display authentication forms in dialog elements
-// navLoginBtn.addEventListener('click', openLoginModal)
-// navSignupBtn.addEventListener('click', openSignupModal)
 
 // navLogoutBtn.addEventListener('click', logOut)
 nav.addEventListener('click', e => {
