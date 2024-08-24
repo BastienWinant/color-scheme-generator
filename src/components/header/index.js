b4ff817dd1e32565a8784b479f2f1b8c978b587f
Bastien Winant <bastien.winant@gmail.com>
2024-08-24 16:46:23 +0200

    show the auth state in the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,12 +3,33 @@ import './style.css'
 import { openLoginModal, logOut } from '../userAuth/login'
 import { openSignupModal } from '../userAuth/signup'
 
+const nav = document.querySelector('#nav')
 const navLoginBtn = document.querySelector('#nav-login-btn')
 const navSignupBtn = document.querySelector('#nav-signup-btn')
 const navLogoutBtn = document.querySelector('#nav-logout-btn')
 
 // display authentication forms in dialog elements
-navLoginBtn.addEventListener('click', openLoginModal)
-navSignupBtn.addEventListener('click', openSignupModal)
+// navLoginBtn.addEventListener('click', openLoginModal)
+// navSignupBtn.addEventListener('click', openSignupModal)
 
-navLogoutBtn.addEventListener('click', logOut)
+// navLogoutBtn.addEventListener('click', logOut)
+nav.addEventListener('click', e => {
+  if (e.target.id == 'nav-login-btn') {
+    openLoginModal()
+  } else if (e.target.id == 'nav-signup-btn') {
+    openSignupModal()
+  } else if (e.target.id == 'nav-logout-btn') {
+    logOut()
+  }
+})
+
+export function showLoginState(user) {
+  if (user) {
+    nav.innerHTML = `
+      <button type="button" id="nav-logout-btn">log out</button>`
+  } else {
+    nav.innerHTML = `
+      <button type="button" id="nav-login-btn">log in</button>
+      <button type="button" id="nav-signup-btn">sign up</button>`
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import './style.css'
 import { onAuthStateChanged } from "firebase/auth"
 
 import { firebaseAuth } from './app'
+import { showLoginState } from './components/header'
 
 async function monitorAuthStatus() {
   onAuthStateChanged(firebaseAuth, user => {
-    if (user) {
-      console.log("logged in!")
-    } else {
-      console.log("logged out!")
-    }
+    showLoginState(user)
   })
 }
 
