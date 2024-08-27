0bc5dd38f27778533de8d105dceb7420a2dd91be
Bastien Winant <bastien.winant@gmail.com>
2024-08-27 09:46:36 +0200

    replace header text with logo img

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,7 @@
 import './style.css'
+import logoImg from './pantone.svg'
 
+const headerLogo = document.querySelector('#header-logo')
 const navExpandBtn = document.querySelector('#nav-expand-btn')
 const headerNavContainer = document.querySelector('#nav-container')
 export const headerNav = document.querySelector('#nav')
@@ -7,32 +9,24 @@ export const headerNav = document.querySelector('#nav')
 export function showLoginState(user) {
   if (user) {
     headerNav.innerHTML = `
-      <div class='header-auth-btns'>
-        <button type="button" id="nav-logout-btn" class="nav-btn header-auth-btn">log out</button>
-      </div>`
+        <button type="button" id="nav-schemes-btn" class="nav-btn">my schemes</button>
+        <button type="button" id="nav-logout-btn" class="nav-btn nav-auth-btn">log out</button>`
   } else {
     headerNav.innerHTML = `
-      <div class='header-auth-btns'>
-        <button type="button" id="nav-login-btn" class="nav-btn nav-auth-btn">log in</button>
-        <button type="button" id="nav-signup-btn" class="nav-btn nav-auth-btn nav-signup-btn">sign up</button>
-      </div>`
+        <button type="button" id="nav-login-btn" class="nav-btn nav-auth-btn nav-login-btn">log in</button>
+        <button type="button" id="nav-signup-btn" class="nav-btn nav-auth-btn">sign up</button>`
   }
 }
 
 function expandNav() {
   headerNavContainer.classList.add('nav-expanded')
 }
+navExpandBtn.addEventListener('click', expandNav)
 
 function collapseNav() {
   headerNavContainer.classList.remove('nav-expanded')
 }
-
-navExpandBtn.addEventListener('click', expandNav)
-
-headerNavContainer.addEventListener('click', e => {
-  if (!e.target.closest('#nav')) {
-    collapseNav()
-  }
-})
-
+headerNavContainer.addEventListener('click', collapseNav)
 window.addEventListener('resize', collapseNav)
+
+headerLogo.src = logoImg
