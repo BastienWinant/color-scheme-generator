0be0e7a9842505dd208954623ff85179346f9675
Bastien Winant <bastien.winant@gmail.com>
2024-09-01 04:10:00 +0200

    fix db emulator connection

--- src/components/color-scheme-generator/dashboard/index.js
+++ src/components/color-scheme-generator/dashboard/index.js
@@ -1,5 +1,12 @@
 import './style.css'
 
+import { ref, push } from "firebase/database"
+
+import { firebaseDB } from '../../../app'
+
+const refInDB = ref(firebaseDB, "userSchemes")
+// TODO: change the permissions on the DB in the console
+
 export const formatOptions = document.querySelectorAll('input[name="format"]')
 const saveSchemeBtn = document.querySelector('#save-scheme-btn')
 const generateCssBtn = document.querySelector('#generate-scheme-btn')
