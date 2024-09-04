fec51d9e35a292f4c18108d54d9bde5a47c45f32
Bastien Winant <bastien.winant@gmail.com>
2024-09-05 01:31:11 +0200

    responsive header and auth modals

--- src/index.js
+++ src/index.js
@@ -30,10 +30,66 @@ if (process.env.NODE_ENV !== 'production') {
 
 // HEADER
 const navExpandBtn = document.querySelector('#nav-expand-btn')
-const nav = document.querySelector('#nav')
+const navContainer = document.querySelector('#nav-container')
+const navAuthExpandBtn = document.querySelector('#nav-auth-expand-btn')
+const navAuthContainer = document.querySelector('#nav-auth-btns-container')
+const navLoginBtn = document.querySelector('#nav-login-btn')
+const cancelLoginBtn = document.querySelector('#cancel-login-btn')
+const navSignupBtn = document.querySelector('#nav-signup-btn')
+const cancelSignupBtn = document.querySelector('#cancel-signup-btn')
 
-function toggleNav() {
-  nav.classList.toggle('nav-expanded')
+// EXPAND/COLLAPSE NAV
+function expandNav() {
+  navContainer.classList.add('nav-expanded')
 }
+navExpandBtn.addEventListener('click', expandNav)
 
-navExpandBtn.addEventListener('click', toggleNav)
+function collapseNav() {
+  navContainer.classList.remove('nav-expanded')
+}
+navContainer.addEventListener('click', e => {
+  if (!e.target.closest('.nav')) {
+    collapseNav()
+  }
+})
+
+// EXPAND/COLLAPSE NAV AUTH
+function expandNavAuth() {
+  navAuthContainer.classList.add('nav-auth-expanded')
+}
+navAuthExpandBtn.addEventListener('click', expandNavAuth)
+
+function collapseNavAuth() {
+  navAuthContainer.classList.remove('nav-auth-expanded')
+}
+navAuthContainer.addEventListener('click', e => {
+  if (!e.target.closest('.nav-auth-btns')) {
+    collapseNavAuth()
+  }
+})
+
+window.addEventListener('resize', () => {
+  collapseNav()
+  collapseNavAuth()
+})
+
+// AUTHENTICATION BUTTONS
+function openLoginModal() {
+  document.querySelector('#login-modal').showModal()
+}
+navLoginBtn.addEventListener('click', openLoginModal)
+
+function closeLoginModal() {
+  document.querySelector('#login-modal').close()
+}
+cancelLoginBtn.addEventListener('click', closeLoginModal)
+
+function openSignupModal() {
+  document.querySelector('#signup-modal').showModal()
+}
+navSignupBtn.addEventListener('click', openSignupModal)
+
+function closeSignupModal() {
+  document.querySelector('#signup-modal').close()
+}
+cancelSignupBtn.addEventListener('click', closeSignupModal)
