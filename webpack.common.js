86fc2abbfdfaf58b70243f506c7472567e070f7d
Bastien Winant <bastien.winant@gmail.com>
2024-09-09 17:57:48 +0200

    set up webpack as 3-page app

---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    generator: ['./src/index.js'],
+    schemes: ['./src/index.js'],
+    colors: ['./src/index.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ["generator"],
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ["schemes"],
+      filename: 'schemes.html',
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ["colors"],
+      filename: 'colors.html',
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
