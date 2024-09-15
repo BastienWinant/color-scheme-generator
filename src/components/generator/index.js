8e5e959b8f1ad4bf391a9cf2ef714182c0a01dc0
Bastien Winant <bastien.winant@gmail.com>
2024-09-15 22:57:02 +0200

    collapse/expand generator form

--- src/components/generator/index.js
+++ src/components/generator/index.js
@@ -1,3 +1,28 @@
 import './style.css'
 
-console.log('this is the generator')
+const generatorForm = document.querySelector('#generator-form')
+
+const generatorInputs = document.querySelector('#generator-inputs')
+const generatorCollapseBtn = document.querySelector('#generator-collapse-btn')
+const generatorSubmitBtn = document.querySelector('#generator-submit-btn')
+
+const schemeControlBtns = document.querySelector('#scheme-ctrl-btns')
+const generatorExpandBtn = document.querySelector('#generator-expand-btn')
+const generatorSaveBtn = document.querySelector('#generator-save-btn')
+
+function collapseGeneratorForm(e) {
+  e.preventDefault()
+
+  generatorInputs.classList.remove('generator-fieldset-expanded')
+  schemeControlBtns.classList.add('generator-fieldset-expanded')
+}
+generatorCollapseBtn.addEventListener('click', collapseGeneratorForm)
+generatorSubmitBtn.addEventListener('click', collapseGeneratorForm)
+
+function expandGeneratorForm(e) {
+  e.preventDefault()
+
+  generatorInputs.classList.add('generator-fieldset-expanded')
+  schemeControlBtns.classList.remove('generator-fieldset-expanded')
+}
+generatorExpandBtn.addEventListener('click', expandGeneratorForm)
