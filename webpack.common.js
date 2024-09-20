c063c9f66fb0117347455a76016497285bed9301
Bastien Winant <bastien.winant@gmail.com>
2024-09-20 15:10:55 +0200

    compilation optimization

--- webpack.common.js
+++ webpack.common.js
@@ -3,25 +3,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    generator: ['./src/components/header/index', './src/components/generator/index'],
+    generator: './src/index.js',
+    schemes: './src/index.js',
+    colors: './src/index.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      chunks: ['generator'],
       title: 'Generator',
-      template: './src/index.html',
+      template: './src/templates/index.html',
+      chunks: ['generator'],
       filename: 'index.html'
     }),
+    new HtmlWebpackPlugin({
+      title: 'Schemes',
+      template: './src/templates/index.html',
+      chunks: ['schemes'],
+      filename: 'schemes.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Colors',
+      template: './src/templates/index.html',
+      chunks: ['colors'],
+      filename: 'colors.html'
+    })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'public'),
     clean: true,
   },
-  resolve: {
-    alias: {
-      Src: path.resolve(__dirname, 'src/'),
-      Components: path.resolve(__dirname, 'src/components/')
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'async',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
   },
   module: {
