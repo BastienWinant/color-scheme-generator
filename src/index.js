6a5dba874b47115cbd316b5dec9860374dc85ee3
Bastien Winant <bastien.winant@gmail.com>
2024-09-20 16:41:15 +0200

    auth modals

--- src/index.js
+++ src/index.js
@@ -27,3 +27,60 @@ if (process.env.NODE_ENV !== 'production') {
   connectAuthEmulator(auth, 'http://localhost:9099')
   connectDatabaseEmulator(db, 'http://localhost', 9000)
 }
+
+// AUTH MODALS
+const loginModal = document.querySelector('#login-modal')
+const loginEmailInput = document.querySelector('#login-email')
+const loginPasswordInput = document.querySelector('#login-password')
+const loginBtn = document.querySelector('#login-btn')
+const switchSignupBtn = document.querySelector('#switch-signup-btn')
+
+const signupModal = document.querySelector('#signup-modal')
+const signupUsernameInput = document.querySelector('#signup-username')
+const signupEmailInput = document.querySelector('#signup-email')
+const signupPasswordInput = document.querySelector('#signup-password')
+const signupBtn = document.querySelector('#signup-btn')
+const switchLoginBtn = document.querySelector('#switch-login-btn')
+
+const openLoginModal = () => {
+  loginModal.showModal()
+}
+
+const closeLoginModal = () => {
+  loginModal.close()
+}
+
+const openSignupModal = () => {
+  signupModal.showModal()
+}
+
+const closeSignupModal = () => {
+  signupModal.close()
+}
+
+loginModal.addEventListener('click', e => {
+  if (!e.target.closest('.auth-form')) closeLoginModal()
+})
+
+signupModal.addEventListener('click', e => {
+  if (!e.target.closest('.auth-form')) closeSignupModal()
+})
+
+loginBtn.addEventListener('click', closeLoginModal)
+signupBtn.addEventListener('click', closeSignupModal)
+
+switchSignupBtn.addEventListener('click', () => {
+  closeLoginModal()
+  openSignupModal()
+})
+switchLoginBtn.addEventListener('click', () => {
+  openLoginModal()
+  closeSignupModal()
+})
+
+// HEADER
+const navLoginBtn = document.querySelector('#nav-login-btn')
+const navSignupBtn = document.querySelector('#nav-signup-btn')
+
+navLoginBtn.addEventListener('click', openLoginModal)
+navSignupBtn.addEventListener('click', openSignupModal)
