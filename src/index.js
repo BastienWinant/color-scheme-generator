3b60af6e4fc3363e7d93560e86c7b21863dbe943
Bastien Winant <bastien.winant@gmail.com>
2024-09-24 23:27:33 +0200

    connect to emulators in dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './style.css'
 
+import { connectAuthEmulator } from 'firebase/auth'
+import { connectDatabaseEmulator } from 'firebase/database'
+
 import { auth, db } from 'Src/app'
 
 if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+  connectAuthEmulator(auth, 'https://localhost:9099')
+  connectDatabaseEmulator(db, "127.0.0.1", 9000)
+
+  console.log('Looks like we are in development mode!')
 }
