aee488e012316bc29c424228be9cdaea0876144e
Bastien Winant <bastien.winant@gmail.com>
2024-09-25 13:20:39 +0200

    implement login/logout functionality

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth"
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth"
 
 import { auth } from "Src/app"
 
@@ -11,6 +11,14 @@ const signupUsername = document.querySelector('#signup-username')
 const signupEmailInput = document.querySelector('#signup-email')
 const signupPasswordInput = document.querySelector('#signup-password')
 const signupBtn = document.querySelector('#signup-btn')
+
+const loginModal = document.querySelector('#login-modal')
+const loginForm = document.querySelector('#login-form')
+const loginFieldset = document.querySelector('#login-fieldset')
+const loginUsername = document.querySelector('#login-username')
+const loginEmailInput = document.querySelector('#login-email')
+const loginPasswordInput = document.querySelector('#login-password')
+const loginBtn = document.querySelector('#login-btn')
 
 export const openSignupModal = () => {
   signupModal.showModal()
@@ -38,3 +46,31 @@ const signupEmailPassword = async (e) => {
 }
 
 signupBtn.addEventListener('click', signupEmailPassword)
+
+export const openLoginModal = () => {
+  loginModal.showModal()
+}
+
+const closeLoginModal = () => {
+  loginModal.close()
+}
+
+const loginEmailPassword = async (e) => {
+  e.preventDefault()
+
+  const email = loginEmailInput.value
+  const password = loginPasswordInput.value
+
+  try {
+    const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+    console.log(userCredentials)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+loginBtn.addEventListener('click', loginEmailPassword)
+
+export const logOut = async () => {
+  signOut(auth)
+}
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,27 @@
 import './style.css'
 
+import { onAuthStateChanged } from 'firebase/auth'
+
 import(/* webpackPrefetch: true */ 'Components/auth')
 
-import { openSignupModal } from 'Components/auth'
+import { auth } from 'Src/app'
+import { openSignupModal, openLoginModal, logOut } from 'Components/auth'
 
 const navSignupBtn = document.querySelector('#nav-signup-btn')
+const navLoginBtn = document.querySelector('#nav-login-btn')
+const navLogoutBtn = document.querySelector('#nav-logout-btn')
 
 navSignupBtn.addEventListener('click', openSignupModal)
+navLoginBtn.addEventListener('click', openLoginModal)
+navLogoutBtn.addEventListener('click', logOut)
+
+const monitorAuthState = () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+    } else {
+      console.log('Logged out!')
+    }
+  })
+}
+monitorAuthState()
