ef8ffb551e54f09677ef4901b8c0bf47c7a466d6
Bastien Winant <bastien.winant@gmail.com>
2024-09-26 16:00:22 +0200

    bypass & delete generator/index file

--- src/entries/index.js
+++ src/entries/index.js
@@ -2,11 +2,33 @@ import { onAuthStateChanged } from 'firebase/auth'
 
 import(/* webpackPrefetch: true */ 'Src/app')
 import(/* webpackPreload: true */ 'Components/header')
-import(/* webpackPreload: true */ 'Components/generator')
+import(/* webpackPreload: true */ 'Components/generator/form')
+import(/* webpackPreload: true */ 'Components/generator/display')
 
 import { auth } from 'Src/app'
 import { showAuthState } from 'Components/header'
-import { initializeDisplay } from 'Components/generator'
+import { initializeColorScheme, updateColorScheme, getSchemeBtn } from 'Components/generator/form'
+import { updateDisplay } from 'Components/generator/display'
+
+getSchemeBtn.addEventListener('click', async e => {
+  e.preventDefault()
+
+  const colorSchemeObj = await updateColorScheme()
+  
+  if (colorSchemeObj) {
+    updateDisplay(colorSchemeObj)
+  }
+})
+
+const initializeDisplay = async () => {
+  // get a color scheme from localStorage or the API
+  const colorSchemeObj = await initializeColorScheme()
+  
+  // render the color scheme data in the DOM
+  if (colorSchemeObj) {
+    updateDisplay(colorSchemeObj)
+  }
+}
 
 const monitorAuthState = () => {
   onAuthStateChanged(auth, user => {
