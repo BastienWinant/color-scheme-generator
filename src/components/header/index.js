ac8926f1d357b1c77106096036ccc8b6d5ca480c
Bastien Winant <bastien.winant@gmail.com>
2024-09-26 15:22:46 +0200

    move auth state monitoring to entry js file

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,7 @@
 import './style.css'
 
-import { onAuthStateChanged } from 'firebase/auth'
-
 import(/* webpackPrefetch: true */ 'Components/auth')
 
-import { auth } from 'Src/app'
 import { openSignupModal, openLoginModal, logOut } from 'Components/auth'
 
 const navBtns = document.querySelector('#nav-btns')
@@ -19,16 +16,13 @@ navBtns.addEventListener('click', e => {
   }
 })
 
-const monitorAuthState = () => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      navBtns.innerHTML = `
-        <button type="button" id="nav-logout-btn" class="nav-btn">log out</button>`
-    } else {
-      navBtns.innerHTML = `
-        <button type="button" id="nav-login-btn" class="nav-btn">log in</button>
-        <button type="button" id="nav-signup-btn" class="nav-btn">sign up</button>`
-    }
-  })
+export const showAuthState = (user) => {
+  if (user) {
+    navBtns.innerHTML = `
+      <button type="button" id="nav-logout-btn" class="nav-btn">log out</button>`
+  } else {
+    navBtns.innerHTML = `
+      <button type="button" id="nav-login-btn" class="nav-btn">log in</button>
+      <button type="button" id="nav-signup-btn" class="nav-btn">sign up</button>`
+  }
 }
-monitorAuthState()
--- src/entries/index.js
+++ src/entries/index.js
@@ -1,3 +1,15 @@
+import { onAuthStateChanged } from 'firebase/auth'
+
 import(/* webpackPrefetch: true */ 'Src/app')
 import(/* webpackPreload: true */ 'Components/header')
 import(/* webpackPreload: true */ 'Components/generator')
+
+import { auth } from 'Src/app'
+import { showAuthState } from 'Components/header'
+
+const monitorAuthState = () => {
+  onAuthStateChanged(auth, user => {
+    showAuthState(user)
+  })
+}
+monitorAuthState()
