68928381b843e24b8083061b824a55b2bbbf9905
Bastien Winant <bastien.winant@gmail.com>
2024-10-16 23:08:52 +0200

    initialize firebase app object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import './style.css'
 
-console.log('Hello World')
+import { initializeApp } from "firebase/app";
 
+let firebaseConfig
 if (process.env.NODE_ENV !== 'production') {
+  // Your web app's Firebase configuration
+  firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  }
   console.log('Looks like we are in development mode!');
 }
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app)
