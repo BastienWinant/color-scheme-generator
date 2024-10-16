f9d40bd79430b2d16bf0100f5e20695d45dd5b34
Bastien Winant <bastien.winant@gmail.com>
2024-10-16 23:34:37 +0200

    add prod firebase config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 
 let firebaseConfig
 if (process.env.NODE_ENV !== 'production') {
@@ -14,9 +14,20 @@ if (process.env.NODE_ENV !== 'production') {
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID
   }
-  console.log('Looks like we are in development mode!');
+
+  console.log('Looks like we are in development mode!')
+} else {
+  firebaseConfig = {
+    apiKey: "AIzaSyCMTcG_LF1bYI2DxJ8FsC7rHzcB8GQXPJM",
+    authDomain: "color-scheme-generator-f60ff.firebaseapp.com",
+    databaseURL: "https://color-scheme-generator-f60ff-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "color-scheme-generator-f60ff",
+    storageBucket: "color-scheme-generator-f60ff.appspot.com",
+    messagingSenderId: "296680716316",
+    appId: "1:296680716316:web:bc35376ef85370755e47d9"
+  }
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 console.log(app)
