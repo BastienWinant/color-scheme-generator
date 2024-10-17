367f414bf10596f6d1c6d66df73390d94dd85096
Bastien Winant <bastien.winant@gmail.com>
2024-10-17 23:18:34 +0200

    add webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -7,4 +8,9 @@ module.exports = merge(common, {
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new Dotenv({
+      path: '.dev.env'
+    })
+  ]
 });
