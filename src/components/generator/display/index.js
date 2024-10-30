79a7d151b3181d8396b1a50daf635e98fcb7b6f4
Bastien Winant <bastien.winant@gmail.com>
2024-10-30 14:26:38 +0100

    implement scheme color removal

--- src/components/generator/display/index.js
+++ src/components/generator/display/index.js
@@ -6,8 +6,14 @@ const generateDisplayElements = (colorsArr) => {
   return colorsArr.map(colorObj => {
     const liEl = document.createElement('li')
     liEl.classList.add('generator-display-color')
+    liEl.dataset.hex = colorObj.hex.clean
 
-    liEl.innerHTML = `${colorObj.name.value}`
+    liEl.innerHTML = `
+      <h2>${colorObj.name.value}</h2>
+      <p>${colorObj.hex.value}</p>
+      <button type="button" class="generator-display-btn save-color-btn">save color</button>
+      <button type="button" class="generator-display-btn remove-color-btn">remove color</button>
+      <button type="button" class="generator-display-btn save-color-btn">save color</button>`
 
     liEl.style.backgroundColor = colorObj.hex.value
 
@@ -21,3 +27,40 @@ export const displayColorScheme = (schemeObj) => {
   generatorDisplay.innerHTML = ''
   generatorDisplay.append(...liArray)
 }
+
+const removeSchemeColor = (schemeObj, e) => {
+  const hex = e.target.closest('.generator-display-color').dataset.hex
+  schemeObj.colors = schemeObj.colors.filter(color => color.hex.clean !== hex)
+  localStorage.setItem('csg-scheme', JSON.stringify(schemeObj))
+  e.target.closest('.generator-display-color').remove()
+
+  const removeColorBtns = document.querySelectorAll('.remove-color-btn')
+
+  if (removeColorBtns.length === 1) {
+    removeColorBtns[0].disabled = true
+  }
+}
+
+const saveSchemeColor = () => {
+  
+}
+
+const copySchemeColor = () => {
+
+}
+
+generatorDisplay.addEventListener('click', e => {
+  const schemeObj = JSON.parse(localStorage.getItem('csg-scheme'))
+
+  if (e.target.classList.contains('save-color-btn')) {
+    const hex = e.target.closest('.generator-display-color').dataset.hex
+    const colorObj = schemeObj.colors.find(color => color.hex.clean === hex)
+    console.log(colorObj)
+  } else if (e.target.classList.contains('copy-color-btn')) {
+    const hex = e.target.closest('.generator-display-color').dataset.hex
+    const colorObj = schemeObj.colors.find(color => color.hex.clean === hex)
+    console.log(colorObj)
+  } else if (e.target.classList.contains('remove-color-btn')) {
+    removeSchemeColor(schemeObj, e)
+  }
+})
