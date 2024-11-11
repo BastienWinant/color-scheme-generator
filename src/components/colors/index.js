4f2809a5bd6a65a83b02361caacd6d3b878b4632
Bastien Winant <bastien.winant@gmail.com>
2024-11-11 21:08:06 +0100

    render color objects

--- src/components/colors/index.js
+++ src/components/colors/index.js
@@ -6,6 +6,36 @@ import { openLoginModal, openSignupModal } from 'Components/auth'
 
 const colorGrid = document.querySelector('#color-grid')
 
+const showNoColorPlaceholder = () => {
+  colorGrid.innerHTML = `
+    <div class="grid-placeholder">
+      <p class="grid-placeholder-text">It's looking a little bland in here...</p>
+      <a href="./index.html">Let's add some colors!</a>
+    </div>`
+}
+
+const showAuthPlaceholder = () => {
+  colorGrid.innerHTML = `
+    <div class="grid-placeholder">
+      <p class="grid-placeholder-text">Log in to save and view colors that inspire you!</p>
+      <div class="grid-auth-btns">
+        <button type="button" id="login-modal-btn" class="login-modal-btn" onclick="openLoginModal()">log in</button>
+        <button type="button" id="signup-modal-btn" class="signup-modal-btn">sign up</button>
+      </div>
+    </div>`
+}
+
+const renderColorGrid = (userColors) => {
+  colorGrid.innerHTML = Object.values(userColors).map(colorObj => {
+    return `<article class="color-card">
+              <hgroup>
+                <h3>${colorObj.name.value}</h3>
+                <p>${colorObj.hex.value}</p>
+              </hgroup>
+            </article>`
+  }).join('\n')
+}
+
 window.addEventListener('click', e => {
   if (e.target.id === 'login-modal-btn') {
     openLoginModal()
@@ -20,23 +50,12 @@ const monitorAuthState = async () => {
       const userColors = await getUserColors()
 
       if (Object.keys(userColors).length) {
-        console.log(userColors)
+        renderColorGrid(userColors)
       } else {
-        colorGrid.innerHTML = `
-          <div class="grid-placeholder">
-            <p class="grid-placeholder-text">It's looking a little bland in here...</p>
-            <a href="./index.html">Let's add some colors!</a>
-          </div>`
+        showNoColorPlaceholder()
       }
     } else {
-      colorGrid.innerHTML = `
-        <div class="grid-placeholder">
-          <p class="grid-placeholder-text">Log in to save and view colors that inspire you!</p>
-          <div class="grid-auth-btns">
-            <button type="button" id="login-modal-btn" class="login-modal-btn" onclick="openLoginModal()">log in</button>
-            <button type="button" id="signup-modal-btn" class="signup-modal-btn">sign up</button>
-          </div>
-        </div>`
+      showAuthPlaceholder()
     }
   })
 }
