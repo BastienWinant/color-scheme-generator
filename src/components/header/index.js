24063f0d9ce2f7fae12069a020c364c69e2407c0
Bastien Winant <bastien.winant@gmail.com>
2024-11-12 13:29:28 +0100

    responsive nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,8 @@ import { openLoginModal, openSignupModal, logOut } from 'Components/auth'
 
 const navToggler = document.querySelector('#nav-toggler')
 const nav = document.querySelector('#nav')
+const navBtnsToggler = document.querySelector('#nav-btns-toggler')
+const navBtns = document.querySelector('#nav-btns')
 const navLoginBtn = document.querySelector('#nav-login-btn')
 const navSignupBtn = document.querySelector('#nav-signup-btn')
 const navLogoutBtn = document.querySelector('#nav-logout-btn')
@@ -13,6 +15,11 @@ const toggleNav = () => {
   nav.classList.toggle('nav-expanded')
 }
 navToggler.addEventListener('click', toggleNav)
+
+const toggleNavBtns = () => {
+  navBtns.classList.toggle('nav-btns-expanded')
+}
+navBtnsToggler.addEventListener('click', toggleNavBtns)
 
 navLoginBtn.addEventListener('click', openLoginModal)
 navSignupBtn.addEventListener('click', openSignupModal)
