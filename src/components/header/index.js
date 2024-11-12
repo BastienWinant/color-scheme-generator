213b820b24bdd90ea3407e17744c616eaf052f19
Bastien Winant <bastien.winant@gmail.com>
2024-11-12 13:10:28 +0100

    collapsible nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,16 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from 'Src/app'
 import { openLoginModal, openSignupModal, logOut } from 'Components/auth'
 
+const navToggler = document.querySelector('#nav-toggler')
+const nav = document.querySelector('#nav')
 const navLoginBtn = document.querySelector('#nav-login-btn')
 const navSignupBtn = document.querySelector('#nav-signup-btn')
 const navLogoutBtn = document.querySelector('#nav-logout-btn')
+
+const toggleNav = () => {
+  nav.classList.toggle('nav-expanded')
+}
+navToggler.addEventListener('click', toggleNav)
 
 navLoginBtn.addEventListener('click', openLoginModal)
 navSignupBtn.addEventListener('click', openSignupModal)
