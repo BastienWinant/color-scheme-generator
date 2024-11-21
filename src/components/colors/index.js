c2c3596d80038078892f7f292450160350869d37
Bastien Winant <bastien.winant@gmail.com>
2024-11-21 19:03:09 +0100

    create color grid

--- src/components/colors/index.js
+++ src/components/colors/index.js
@@ -2,47 +2,26 @@ import './index.css'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from 'Src/app'
 import { getUserColors } from 'Src/utils'
-import { openLoginModal, openSignupModal } from 'Components/auth'
 
 const colorGrid = document.querySelector('#color-grid')
 
-const showNoColorPlaceholder = () => {
-  colorGrid.innerHTML = `
-    <div class="grid-placeholder">
-      <p class="grid-placeholder-text">It's looking a little bland in here...</p>
-      <a href="./index.html">Let's add some colors!</a>
-    </div>`
-}
-
-const showAuthPlaceholder = () => {
-  colorGrid.innerHTML = `
-    <div class="grid-placeholder">
-      <p class="grid-placeholder-text">Log in to save and view colors that inspire you!</p>
-      <div class="grid-auth-btns">
-        <button type="button" id="login-modal-btn" class="login-modal-btn" onclick="openLoginModal()">log in</button>
-        <button type="button" id="signup-modal-btn" class="signup-modal-btn">sign up</button>
-      </div>
-    </div>`
-}
-
 const renderColorGrid = (userColors) => {
   colorGrid.innerHTML = Object.values(userColors).map(colorObj => {
-    return `<article class="color-card">
-              <hgroup>
-                <h3>${colorObj.name.value}</h3>
-                <p>${colorObj.hex.value}</p>
-              </hgroup>
-            </article>`
+    return `<figure class="color-card">
+      <img class="color-card-img" src="${colorObj.image.bare}" alt="Color name and background." />
+      <figcaption class="color-card-caption">
+        <hgroup>
+          <h2>${colorObj.name.value}</h2>
+          <p>${colorObj.hex.clean}</p>
+        </hgroup>
+        <section class="color-card-btns">
+          <button type="button" class="color-card-btn color-copy-btn">copy</button>
+          <button type="button" class="color-card-btn color-delete-btn">remove</button>
+        </section>
+      </figcaption>
+    </figure>`
   }).join('\n')
 }
-
-window.addEventListener('click', e => {
-  if (e.target.id === 'login-modal-btn') {
-    openLoginModal()
-  } else if (e.target.id === 'signup-modal-btn') {
-    openSignupModal()
-  }
-})
 
 const monitorAuthState = async () => {
   onAuthStateChanged(auth, async (user) => {
@@ -50,12 +29,16 @@ const monitorAuthState = async () => {
       const userColors = await getUserColors()
 
       if (Object.keys(userColors).length) {
+        console.log(Object.values(userColors))
         renderColorGrid(userColors)
+        // console.log(userColors)
       } else {
-        showNoColorPlaceholder()
+        // showNoColorPlaceholder()
+        console.log('no color saved')
       }
     } else {
-      showAuthPlaceholder()
+      // showAuthPlaceholder()
+      console.log('not logged id')
     }
   })
 }
