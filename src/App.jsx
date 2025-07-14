5c6ec6ebf8b7bfca3aaa853aa4afd3a70571fb14
BastienWinant <bastien.winant@gmail.com>
2025-07-15 01:12:57 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import './App.css'
 
 function App() {
   return (
-    <>this is the ShadeMaker app</>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>this is the home page</h1>} />
+          <Route path="/user" element={<h1>this is the user page</h1>} />
+        </Routes>
+      </Router>
   )
 }
 
