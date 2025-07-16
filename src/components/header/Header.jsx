64d9d3068bd1807c958d96ac5db626a5c0a8525b
BastienWinant <bastien.winant@gmail.com>
2025-07-16 22:04:05 +0200

    create auth form button and dialogs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
-import { Box } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Dialog,
+} from "@chakra-ui/react";
+import SignupDialog from "@/components/signupDialog/SignupDialog.jsx";
+import SigninDialog from "@/components/signinDialog/SigninDialog.jsx";
 
 export default function Header() {
 	return (
-			<Box as="header">
-				this is the header
+			<Box as="header" borderWidth="medium">
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<Button variant="outline">
+							sign up
+						</Button>
+					</Dialog.Trigger>
+					<SignupDialog />
+				</Dialog.Root>
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<Button variant="outline">
+							sign in
+						</Button>
+					</Dialog.Trigger>
+					<SigninDialog />
+				</Dialog.Root>
 			</Box>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { Provider } from "@/components/ui/provider"
-import { AuthUserProvider } from "@/contexts/authContext/authUserProvider.jsx";
+import AuthUserProvider from "@/contexts/authContext/authUserProvider.jsx";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
