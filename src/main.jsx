270af3c76ceb8d5298012eba63ae9376ee01e0b8
BastienWinant <bastien.winant@gmail.com>
2025-07-16 21:46:20 +0200

    wrap app in Auth User Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { Provider } from "@/components/ui/provider"
+import { AuthUserProvider } from "@/contexts/authContext/authUserProvider.jsx";
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-		<Provider>
-			<App />
-		</Provider>
+		<AuthUserProvider>
+			<Provider>
+				<App />
+			</Provider>
+		</AuthUserProvider>
   </StrictMode>,
 )
