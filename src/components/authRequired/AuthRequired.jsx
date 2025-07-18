37e32f7825d0b2c14395c3fdaa4da99cca12bff6
BastienWinant <bastien.winant@gmail.com>
2025-07-18 11:20:00 +0200

    fix protected route

--- src/components/authRequired/AuthRequired.jsx
+++ src/components/authRequired/AuthRequired.jsx
@@ -1,11 +1,13 @@
 import { Outlet, Navigate } from "react-router";
 import { useAuth } from "@/contexts/authContext/authUserContext.js";
-export default function AuthRequired() {
-	const { authObj } = useAuth();
+import {Spinner} from "@chakra-ui/react";
 
-	if (true) {
-		return <Navigate to="/" />
+export default function AuthRequired() {
+	const { authUser, isLoading } = useAuth();
+
+	if (!isLoading && !authUser) {
+		return <Navigate to="/" />;
 	}
 
-	return <Outlet />
+	return <>{isLoading ? <Spinner size="xl" /> : <Outlet />}</>
 }
