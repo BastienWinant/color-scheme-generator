2c895e0044448ad584d7aaf70e08132d0a4c25d2
BastienWinant <bastien.winant@gmail.com>
2025-07-28 14:15:40 +0200

    new home page component

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import Generator from "@/components/generator/index.jsx";
+
+export default function Home() {
+	return (
+			<Box as="main">
+				<Generator />
+			</Box>
+	)
+}
