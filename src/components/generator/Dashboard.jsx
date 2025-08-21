82b95525372633fef4dcc78d5e01955830764d44
BastienWinant <bastien.winant@gmail.com>
2025-08-21 22:47:47 +0200

    close form on new scheme

--- src/components/generator/Dashboard.jsx
+++ src/components/generator/Dashboard.jsx
@@ -8,10 +8,19 @@ import {
 } from "@chakra-ui/react"
 import { FaHeart } from "react-icons/fa6";
 import Form from "./form/Form.jsx";
+import {useEffect, useState} from "react";
+import { useColorSchemeContext } from "@/contexts/colorSchemeContext/ColorSchemeContext.js";
 
 export default function Dashboard() {
+	const [open, setOpen] = useState(false)
+	const { colorScheme } = useColorSchemeContext()
+
+	useEffect(() => {
+		setOpen(false)
+	}, [colorScheme]);
+
 	return (
-		<Drawer.Root placement="bottom">
+		<Drawer.Root placement="bottom" open={open} onOpenChange={(e) => setOpen(e.open)}>
 			<HStack borderWidth="medium">
 				<Drawer.Trigger flexGrow="1" asChild>
 					<Button variant="outline" size="xl">
