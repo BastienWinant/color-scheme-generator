1bd51872a2a5bcbbd2268f3709a8b550cbf19045
BastienWinant <bastien.winant@gmail.com>
2025-09-08 16:58:23 +0200

    mode select component

--- src/components/generator/form/GeneratorForm.jsx
+++ src/components/generator/form/GeneratorForm.jsx
@@ -1,11 +1,13 @@
 import { Stack, Field, Input, Button } from "@chakra-ui/react"
 import ColorInput from "@/components/generator/form/ColorInput.jsx"
+import ModeSelect from "@/components/generator/form/ModeSelect.jsx";
 
 export default function GeneratorForm() {
 	return (
 		<form>
 			<Stack borderWidth="thin">
 				<ColorInput />
+				<ModeSelect />
 				<Button variant="solid">Submit</Button>
 			</Stack>
 		</form>
