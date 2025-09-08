ce9ca17afe7df528c49e22b03abe09e53e95f408
BastienWinant <bastien.winant@gmail.com>
2025-09-08 17:30:15 +0200

    mobile stepper

--- src/components/generator/form/GeneratorForm.jsx
+++ src/components/generator/form/GeneratorForm.jsx
@@ -1,6 +1,7 @@
 import { Stack, Field, Input, Button } from "@chakra-ui/react"
 import ColorInput from "@/components/generator/form/ColorInput.jsx"
-import ModeSelect from "@/components/generator/form/ModeSelect.jsx";
+import ModeSelect from "@/components/generator/form/ModeSelect.jsx"
+import CountInput from "@/components/generator/form/NumberInput.jsx";
 
 export default function GeneratorForm() {
 	return (
@@ -8,6 +9,7 @@ export default function GeneratorForm() {
 			<Stack>
 				<ColorInput />
 				<ModeSelect />
+				<CountInput />
 				<Button variant="solid">Submit</Button>
 			</Stack>
 		</form>
--- src/components/generator/form/NumberInput.jsx
+++ src/components/generator/form/NumberInput.jsx
@@ -1,12 +1,24 @@
-import { Field, NumberInput } from "@chakra-ui/react"
+import { Field, NumberInput, HStack, IconButton } from "@chakra-ui/react"
+import { FaPlus, FaMinus } from "react-icons/fa6"
 
 export default function CountInput() {
   return (
     <Field.Root>
       <Field.Label>Enter Number</Field.Label>
-      <NumberInput.Root defaultValue="5">
-        <NumberInput.Control />
-        <NumberInput.Input />
+      <NumberInput.Root defaultValue="3" unstyled spinOnPress={false}>
+        <HStack gap="2">
+          <NumberInput.DecrementTrigger asChild>
+            <IconButton variant="outline" size="sm">
+              <FaMinus />
+            </IconButton>
+          </NumberInput.DecrementTrigger>
+          <NumberInput.ValueText textAlign="center" fontSize="lg" minW="3ch" />
+          <NumberInput.IncrementTrigger asChild>
+            <IconButton variant="outline" size="sm">
+              <FaPlus />
+            </IconButton>
+          </NumberInput.IncrementTrigger>
+        </HStack>
       </NumberInput.Root>
     </Field.Root>
   )
