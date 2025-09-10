77867cbf5d9828a357a3e293ae817ae947c45896
BastienWinant <bastien.winant@gmail.com>
2025-09-10 20:07:32 +0200

    use NativeSelect

--- src/components/generator/form/ModeSelect.jsx
+++ src/components/generator/form/ModeSelect.jsx
@@ -1,31 +1,23 @@
-import { Select } from "@chakra-ui/react"
-import {useColorSchemeContext} from "@/contexts/colorSchemeContext/ColorSchemeContext.js"
+"use client"
 
-export default function ModeSelect() {
-  const { modes } = useColorSchemeContext()
+import { NativeSelect } from "@chakra-ui/react"
+import { useState } from "react"
 
+export default function SelectMode() {
+  const [value, setValue] = useState("")
   return (
-    <Select.Root collection={modes}>
-      <Select.HiddenSelect />
-      <Select.Label>Select mode</Select.Label>
-      <Select.Control>
-        <Select.Trigger>
-          <Select.ValueText placeholder="Select mode" />
-        </Select.Trigger>
-        <Select.IndicatorGroup>
-          <Select.Indicator />
-        </Select.IndicatorGroup>
-      </Select.Control>
-      <Select.Positioner>
-        <Select.Content>
-          {modes.items.map(item => (
-            <Select.Item item={item} key={item.value}>
-              {item.label}
-              <Select.ItemIndicator />
-            </Select.Item>
-          ))}
-        </Select.Content>
-      </Select.Positioner>
-    </Select.Root>
+    <NativeSelect.Root size="sm" width="240px">
+      <NativeSelect.Field
+        placeholder="Select option"
+        value={value}
+        onChange={(e) => setValue(e.currentTarget.value)}
+      >
+        <option value="react">React</option>
+        <option value="vue">Vue</option>
+        <option value="angular">Angular</option>
+        <option value="svelte">Svelte</option>
+      </NativeSelect.Field>
+      <NativeSelect.Indicator />
+    </NativeSelect.Root>
   )
 }
