a3681a30339d4116d050aec53c98188918580161
BastienWinant <bastien.winant@gmail.com>
2025-10-21 21:54:26 +0200

    implement header navigation

--- src/components/header/userMenu/UserMenu.jsx
+++ src/components/header/userMenu/UserMenu.jsx
@@ -1,14 +1,16 @@
 import { Button, Menu, Portal } from "@chakra-ui/react"
 import { useAuth } from "@/contexts/auth/AuthUserContext.js"
 import { FaCircleUser } from "react-icons/fa6"
+import { useNavigate } from "react-router"
 
 export default function UserMenu() {
 	const { authUser, signOut } = useAuth()
+	const navigate = useNavigate()
 
 	return (
 		<Menu.Root>
 			<Menu.Trigger asChild>
-				<Button variant="plain" _focus={{ outline: 0}}>
+				<Button variant="outline" _focus={{ outline: 0}}>
 					<FaCircleUser />
 					{ authUser.username }
 				</Button>
@@ -17,9 +19,9 @@ export default function UserMenu() {
 				<Menu.Positioner>
 					<Menu.Content>
 						<Menu.ItemGroup>
-							<Menu.Item value="generator">generator</Menu.Item>
-							<Menu.Item value="schemes">schemes</Menu.Item>
-							<Menu.Item value="colors">colors</Menu.Item>
+							<Menu.Item value="generator" onClick={() => navigate("/")}>generator</Menu.Item>
+							<Menu.Item value="schemes" onClick={() => navigate("protected")}>schemes</Menu.Item>
+							<Menu.Item value="colors" onClick={() => navigate("protected")}>colors</Menu.Item>
 						</Menu.ItemGroup>
 						<Menu.Separator />
 						<Menu.ItemGroup>
