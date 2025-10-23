df2e40649ea4bba0fd9103bebd633c10d996bcf0
BastienWinant <bastien.winant@gmail.com>
2025-10-23 04:36:58 +0200

    clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [jsconfigPaths(), react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
 })
